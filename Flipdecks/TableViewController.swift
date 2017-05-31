a97279623c9a4afdd2ae60a2903cec95ef4be01c
Sabine Kaupp <sabinekaupp@dhcp-10-180-123-107.dynamic.eduroam.mwn.de>
2017-05-31 13:06:26 +0200

    SECOND COMMIT

--- Flipdecks/TableViewController.swift
+++ Flipdecks/TableViewController.swift
@@ -12,6 +12,8 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSLog("my changes")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
