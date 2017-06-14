ebfd6cdb9ba5edebf41543f4fd27280fff6a8441
Nicola Greth <nicolagreth@Nicolas-MacBook-Air.local>
2017-06-14 20:31:46 +0200

    Added list functionality (search not working yet)

---
+++ FlipDecks/FlipDecks/ListAndSearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndSearchTableViewCell.swift
+//  FlipDecks
+//
+//  Created by Nicola Greth on 14.06.17.
+//  Copyright © 2017 Nicola Greth. All rights reserved.
+//
+
+import UIKit
+
+class ListAndSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var AnswerLabel: UILabel!
+    @IBOutlet weak var QuestionLabel: UILabel!
+
+
+}
--- FlipDecks/FlipDecks/ListAndSearchTableViewController.swift
+++ FlipDecks/FlipDecks/ListAndSearchTableViewController.swift
@@ -13,10 +13,15 @@ class ListAndSearchTableViewController: UITableViewController {
     // current deck and language
     var deck : Deck = Deck(name: "", languageName: "", fileEnding: "")
     var language : Language = Language(name: "")
-    
+    var listOfCards = [Card]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.listOfCards = self.deck.returnAllCards()
+    }
+    
+    func editingChanged(_ SearchBar: UISearchBar) {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,68 +29,18 @@ class ListAndSearchTableViewController: UITableViewController {
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.deck.getCountOfCards()
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ListAndSearchTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListEntryCell", for: indexPath) as! ListAndSearchTableViewCell
+        cell.QuestionLabel?.text = self.listOfCards[indexPath.row].getQuestion()
+        cell.AnswerLabel?.text = self.listOfCards[indexPath.row].getQuestion()
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
