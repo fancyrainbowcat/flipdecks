92f107176b6140fe431de657091708145656ef82
Nicola Greth <nicolagreth@Nicolas-MacBook-Air.local>
2017-06-14 22:57:08 +0200

    Added search functionality

--- FlipDecks/FlipDecks/ListAndSearchTableViewController.swift
+++ FlipDecks/FlipDecks/ListAndSearchTableViewController.swift
@@ -8,20 +8,35 @@
 
 import UIKit
 
+extension ListAndSearchTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+}
+
 class ListAndSearchTableViewController: UITableViewController {
 
     // current deck and language
     var deck : Deck = Deck(name: "", languageName: "", fileEnding: "")
     var language : Language = Language(name: "")
     var listOfCards = [Card]()
+    var filteredCards = [Card]()
+    let searchController = UISearchController(searchResultsController: nil)
+
         
     override func viewDidLoad() {
         super.viewDidLoad()
         self.listOfCards = self.deck.returnAllCards()
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        self.tableView.tableHeaderView = searchController.searchBar
     }
+
     
-    func editingChanged(_ SearchBar: UISearchBar) {
-        
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredCards = listOfCards.filter { $0.getQuestion().lowercased().contains(searchText.lowercased()) || $0.getAnswer().lowercased().contains(searchText.lowercased())}
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,16 +46,25 @@ class ListAndSearchTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.deck.getCountOfCards()
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return self.filteredCards.count
+        }
+        return self.listOfCards.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ListAndSearchTableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListEntryCell", for: indexPath) as! ListAndSearchTableViewCell
-        cell.QuestionLabel?.text = self.listOfCards[indexPath.row].getQuestion()
-        cell.AnswerLabel?.text = self.listOfCards[indexPath.row].getQuestion()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! ListAndSearchTableViewCell
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            cell.QuestionLabel?.text = self.filteredCards[indexPath.row].getQuestion()
+            cell.AnswerLabel?.text = self.filteredCards[indexPath.row].getAnswer()
+        } else {
+            cell.QuestionLabel?.text = self.listOfCards[indexPath.row].getQuestion()
+            cell.AnswerLabel?.text = self.listOfCards[indexPath.row].getAnswer()
+        }
         return cell
     }
 }
