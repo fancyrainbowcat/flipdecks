208c0a288dde4a7a5075db8e3a978855d479d920
Sabine Kaupp <sabinekaupp@Sabines-MacBook-Air.local>
2017-06-22 15:52:51 +0200

    Updated Quiz Mode

--- FlipDecks/FlipDecks/QuizViewController.swift
+++ FlipDecks/FlipDecks/QuizViewController.swift
@@ -15,7 +15,12 @@ class QuizViewController: UIViewController {
     var language : Language = Language(name: "")
     var array = [String] ()
     var strValue = ""
+    var strValueOne = ""
+    var strValueTwo = ""
+    var strValueThree = ""
+    var strValueFour = ""
     var currentCardIndex = 0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,30 +33,174 @@ class QuizViewController: UIViewController {
         
         deck.listOfCards.shuffle()
         printQuestion()
+        ShelveQuiz.isHidden = false
+        NextQuiz.isHidden = true
+        QuizAnswerView.isHidden = true
+        QuizQuestionView.isHidden = false
         
         //to determine when the Application is entering into background
         NotificationCenter.default.addObserver(self, selector: #selector(willEnterBackground), name: .UIApplicationDidEnterBackground, object: nil)
         
         //to determine when the Application is terminated
         NotificationCenter.default.addObserver(self, selector: #selector(willTerminate), name: .UIApplicationWillTerminate, object: nil)
-
+        
     }
-    
-    @IBOutlet weak var QuestionQuizLabel: UILabel!
     
     //Print question on label
     @IBAction func printQuestion() {
-        strValue = deck.listOfCards[currentCardIndex].getQuestion()
-        QuestionQuizLabel.text = strValue
-    }
+        if (currentCardIndex < deck.getCountOfCards()) {
+            strValue = deck.listOfCards[currentCardIndex].getQuestion()
+            QuestionQuizLabel.text = strValue
+            pickRandomAnswer()
+            }
+        else {
+            strValue = "Congratulations!"
+            QuestionQuizLabel.text = strValue
+            AnswerOneLabel.isHidden = true
+            AnswerTwoLabel.isHidden = true
+            AnswerThreeLabel.isHidden = true
+            AnswerFourLabel.isHidden = true
+            NextQuiz.isHidden = true
+            AnswerOne.isHidden = true
+            AnswerTwo.isHidden = true
+            AnswerThree.isHidden = true
+            AnswerFour.isHidden = true
+            }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
+    //Print answer on label
+    @IBAction func printAnswer() {
+        strValue = deck.listOfCards[currentCardIndex].getAnswer()
+        AnswerQuizLabel.text = strValue
+        ShelveQuiz.isHidden = true
+        NextQuiz.isHidden = false
+        flip()
+    }
 
+    // Play next Card
+    @IBAction func playNextCard() {
+        currentCardIndex += 1
+        QuizQuestionView.isHidden = false
+        QuizAnswerView.isHidden = true
+        printQuestion()
+        AnswerOne.isHidden = false
+        AnswerTwo.isHidden = false
+        AnswerThree.isHidden = false
+        AnswerFour.isHidden = false
+    }
+    
+    
+    // pick random answers
+    func pickRandomAnswer() {
+    var interrimArray = deck.listOfCards
+        interrimArray.remove(at: currentCardIndex)
+    var randomAnswerArray = interrimArray.choose(3)
+    randomAnswerArray.append(deck.listOfCards[currentCardIndex])
+    randomAnswerArray.shuffle()
+    strValueOne = randomAnswerArray[0].getAnswer()
+    AnswerOneLabel.text = strValueOne
+    strValueTwo = randomAnswerArray[1].getAnswer()
+    AnswerTwoLabel.text = strValueTwo
+    strValueThree = randomAnswerArray[2].getAnswer()
+    AnswerThreeLabel.text = strValueThree
+    strValueFour = randomAnswerArray[3].getAnswer()
+    AnswerFourLabel.text = strValueFour
+    }
+
+    /*/ Select Answers
+    func selectAnswer() {
+        let tapOne = UITapGestureRecognizer(target: self, action: #selector(checkCardOne))
+    AnswerOneLabel.isUserInteractionEnabled = true
+    AnswerOneLabel.addGestureRecognizer(tapOne)
+    AnswerTwoLabel.isUserInteractionEnabled = true
+    AnswerThreeLabel.isUserInteractionEnabled = true
+    AnswerFourLabel.isUserInteractionEnabled = true
+    }
+ */
+    
+    // Check Card One
+    @IBAction func checkCardOne() {
+        if (deck.listOfCards[currentCardIndex].getAnswer() == AnswerOneLabel.text) {
+        deck.listOfCards[currentCardIndex].cardPlayed(result: "correct")
+        }
+        else {
+        deck.listOfCards[currentCardIndex].cardPlayed(result: "")
+        }
+        printAnswer()
+        AnswerOne.isHidden = true
+        AnswerTwo.isHidden = true
+        AnswerThree.isHidden = true
+        AnswerFour.isHidden = true
+   }
+    
+    // Check Card Two
+    @IBAction func checkCardTwo() {
+        if (deck.listOfCards[currentCardIndex].getAnswer() == AnswerTwoLabel.text) {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "correct")
+        }
+        else {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "")
+        }
+        printAnswer()
+        AnswerOne.isHidden = true
+        AnswerTwo.isHidden = true
+        AnswerThree.isHidden = true
+        AnswerFour.isHidden = true
+    }
+    
+    
+    // Check Card Three
+    @IBAction func checkCardThree() {
+        if (deck.listOfCards[currentCardIndex].getAnswer() == AnswerThreeLabel.text) {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "correct")
+        }
+        else {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "")
+        }
+        printAnswer()
+        AnswerOne.isHidden = true
+        AnswerTwo.isHidden = true
+        AnswerThree.isHidden = true
+        AnswerFour.isHidden = true
+    }
+    
+    // Check Card Four
+    @IBAction func checkCardFour() {
+        if (deck.listOfCards[currentCardIndex].getAnswer() == AnswerFourLabel.text) {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "correct")
+        }
+        else {
+            deck.listOfCards[currentCardIndex].cardPlayed(result: "")
+        }
+        printAnswer()
+        AnswerOne.isHidden = true
+        AnswerTwo.isHidden = true
+        AnswerThree.isHidden = true
+        AnswerFour.isHidden = true
+    }
+    
+    
+    //Flip Label
+    func flip() {
+        let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+        
+        UIView.transition(with: QuizQuestionView, duration: 1.0, options: transitionOptions, animations: {
+            self.QuizQuestionView.isHidden = true
+        })
+        
+        UIView.transition(with: QuizAnswerView, duration: 1.0, options: transitionOptions, animations: {
+            self.QuizAnswerView.isHidden = false
+        })
+        ShelveQuiz.isHidden = true
+        NextQuiz.isHidden = false
+    }
+    
+    
     
     @IBOutlet weak var ShelveQuiz: UIButton!
     
@@ -79,12 +228,23 @@ class QuizViewController: UIViewController {
         self.deck.saveToFile()
     }
     
+    @IBOutlet weak var AnswerOneLabel: UILabel!
+    @IBOutlet weak var AnswerTwoLabel: UILabel!
     @IBOutlet weak var AnswerOne: UIButton!
+    @IBOutlet weak var AnswerThreeLabel: UILabel!
+    @IBOutlet weak var AnswerFourLabel: UILabel!
     @IBOutlet weak var AnswerTwo: UIButton!
     @IBOutlet weak var AnswerThree: UIButton!
     @IBOutlet weak var AnswerFour: UIButton!
     
+    @IBOutlet weak var QuizAnswerView: UIView!
+    @IBOutlet weak var QuizQuestionView: UIView!
     
+    @IBOutlet weak var NextQuiz: UIButton!
+    
+    @IBOutlet weak var AnswerQuizLabel: UILabel!
+    
+    @IBOutlet weak var QuestionQuizLabel: UILabel!
     
     //function that will be called once the app is terminated
     func willTerminate(_ notification: Notification) {
