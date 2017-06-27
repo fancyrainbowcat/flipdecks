46e7f4fcea3ea7f3633e85f634c0061adf60fd0c
mazlani <mazlani-zouhair@hotmail.com>
2017-06-27 12:12:51 +0200

    old BarChart File D

---
+++ FlipDecks/FlipDecks/BarChartViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BarChartViewController.swift
+//  FlipDecks
+//
+//  Created by admin on 26.06.17.
+//  Copyright © 2017 Nicola Greth. All rights reserved.
+//
+
+import UIKit
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
---
+++ FlipDecks/FlipDecks/BarChartViewController1.swift
@@ -0,0 +1,57 @@
+//
+//  BarChartViewController1.swift
+//  FlipDecks
+//
+//  Created by admin on 26.06.17.
+//  Copyright © 2017 Nicola Greth. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+
+var answer: [String]!
+
+
+class BarChartViewController1: UIViewController {
+@IBOutlet weak var barChartView: BarChartView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        answer = ["Right", "False"]
+        let units = [20.0, 4.0]
+        
+        
+        setChart(dataPoints: answer, values: units)
+
+         }
+        
+        func setChart(dataPoints: [String], values: [Double]) {
+            barChartView.noDataText = "There is no Data"
+            
+            var dataEntries: [BarChartDataEntry] = []
+            
+            for i in 0..<dataPoints.count
+            {
+                let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+                dataEntries.append(dataEntry)
+            }
+            
+            let linechartdataset=BarChartDataSet(values: dataEntries, label: "Questions")
+            var datasets=[IChartDataSet]()
+            datasets.append(linechartdataset)
+            let linechartdata=BarChartData(dataSets: datasets)
+            barChartView.data = linechartdata
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
